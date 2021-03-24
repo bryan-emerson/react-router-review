f9e87b539cbab26fd26ec18f216393d1a9b10d55
maker-jws <ws.joshua@gmail.com>
2021-03-24 13:52:22 -0600

    cleaned up cra and added nav

---
+++ src/Components/App/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+import Navigation from '../Nav'
+class App extends React.Component{
+  
+  render(){
+    return (
+      <div className="App">
+        <Navigation/>
+        APP
+      </div>
+    );
+  }
+  
+}
+
+export default App;
